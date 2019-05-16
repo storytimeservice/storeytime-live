7a0aa200aeb175841138fa4c10eef3b00bcd0b38
reguv760 <reguv760@gmail.com>
2019-05-15 23:07:03 -0700

    register testimonials preview to CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,19 @@
 import CMS from 'netlify-cms'
 
 import DonatePreview from './preview-templates/DonatePreview'
+import TestimonialPreview from './preview-templates/TestimonialsListPreview'
 import EventPreview from './preview-templates/EventPreview'
 import HomePreview from './preview-templates/HomePreview'
 import SingerListPreview from './preview-templates/SingerListPreview'
 import VideoListPreview from './preview-templates/VideoListPreview'
+
 import siteStyle from './../assets/scss/main.scss'
 
 CMS.registerPreviewStyle(siteStyle)
 
 //preview template names has to match names in config.yml
 CMS.registerPreviewTemplate('donate', DonatePreview)
+CMS.registerPreviewTemplate('testimonialList', TestimonialPreview)
 CMS.registerPreviewTemplate('eventsList', EventPreview)
 CMS.registerPreviewTemplate('home', HomePreview)
 CMS.registerPreviewTemplate('singerList', SingerListPreview)
